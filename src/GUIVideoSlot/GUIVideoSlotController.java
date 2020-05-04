14aad866ec9d1102d1672b115c1f66d8c84911bc
deniskuzner <deniskuzner@gmail.com>
2020-05-04 14:54:10 +0200

    Inicijalizacija panela iz memorije

--- src/GUIVideoSlot/GUIVideoSlotController.java
+++ src/GUIVideoSlot/GUIVideoSlotController.java
@@ -5,14 +5,21 @@
  */
 package GUIVideoSlot;
 
+import Domain.Symbol;
 import GUIVideoSlot.Listeners.MinusListener;
 import GUIVideoSlot.Listeners.PlusListener;
 import GUIVideoSlot.Listeners.SpinListener;
 import Session.Session;
 import Transfer.TransferObject;
+import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 /**
  *
@@ -26,6 +33,9 @@ public final class GUIVideoSlotController {
     int betValuesIndex;
     int win;
     int mat[][];
+    List<Symbol> symbols;
+    //List<Position> positions;
+    //List<SPosition> sPositions = new ArrayList(); nece biti ovde nego u metodi spin() !!!
 
     public GUIVideoSlotController(FXMLDocumentController fxmlDocumentController) {
         this.fxmlDocumentController = fxmlDocumentController;
@@ -38,8 +48,21 @@ public final class GUIVideoSlotController {
         betValuesIndex = 0;
         win = 0;
         mat = new int[3][5];
+        getSymbols();
+        //getPositions();
 
         populateForm();
+    }
+
+    void getSymbols() {
+        Symbol s1 = new Symbol(1, "lemon");
+        Symbol s2 = new Symbol(2, "orange");
+        Symbol s3 = new Symbol(3, "cherry");
+        Symbol s4 = new Symbol(4, "grapes");
+        Symbol s5 = new Symbol(5, "melon");
+        Symbol s6 = new Symbol(6, "bell");
+        Symbol s7 = new Symbol(7, "seven");
+        symbols = Arrays.asList(s1, s2, s3, s4, s5, s6, s7);
     }
 
     void populateForm() {
@@ -79,23 +102,56 @@ public final class GUIVideoSlotController {
 
     //TODO
     public void spin() {
+        
+        initializePanel();
+        
+        
+        
         //TODO: LOGIKA CELE IGRE
 
         //postaviti na disabled spin, plus i minus btn dok se ne zavrsi spin, pa onda vratiti
+        //1.update user - skinuti mu bet sa balansa
+        //2.popuniti matricu random vrednostima
+        //3.napraviti sPosition(id, symbol, position) objekat za svako polje u matrici i sacuvati ih sve u bazi
     }
 
-    private void randomizeMatValues() {
+    //ovo ce ici u poslovnu logiku? pa odatle uraditi setMat() za ovu klasu
+    void randomizeMatValues() {
         for (int x = 0; x < 3; x++) {
             for (int y = 0; y < 5; y++) {
-                mat[x][y] = new Random().nextInt(8);
+                mat[x][y] = new Random().nextInt(symbols.size());
                 System.out.println("MAT[" + x + "][" + y + "]: " + mat[x][y]);
             }
         }
     }
 
     //TODO
-    private void setImages() {
+    void setImages() {
+        for (int x = 0; x < 3; x++) {
+            for (int y = 0; y < 5; y++) {
+                setImage(getImage(symbols.get(mat[x][y]).getName()), x, y);
+            }
+        }
+    }
 
+    void setImage(Image image, int x, int y) {
+        Button button = null;
+        String buttonName = "p" + x + y;
+        Class fxmlDocumentControllerClass = this.fxmlDocumentController.getClass();
+        try {
+            Field field = fxmlDocumentControllerClass.getDeclaredField(buttonName);
+            button = (Button) field.get(this.fxmlDocumentController);
+        } catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException ex) {
+            Logger.getLogger(GUIVideoSlotController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        button.setGraphic(new ImageView(image));
+    }
+
+    Image getImage(String name) {
+        String location = "assets/images/";
+        String fileName = name + ".png";
+        return new Image(location + fileName, 140, 140, false, true);
     }
 
 }
